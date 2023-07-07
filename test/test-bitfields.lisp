152790e3ed9b9fac9d0dbdb367bb22253f87cb87
Simon Dobson <simon.dobson@computer.org>
2023-07-07 17:55:10 +0100

    Missing line in header

--- test/test-bitfields.lisp
+++ test/test-bitfields.lisp
@@ -15,6 +15,7 @@
 ;; GNU General Public License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
+;; along with cl-vhdsl. If not, see <http://www.gnu.org/licenses/gpl.html>.
 
 (in-package :cl-vhdsl-test)
 (require :cl-vhdsl)
