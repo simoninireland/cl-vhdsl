af0f1bbbbbd259a06329f0fe6a11f463180078ac
Simon Dobson <simon.dobson@computer.org>
2024-08-12 17:48:48 +0100

    New tests for connectors and bus

--- test/test-wiring.lisp
+++ test/test-wiring.lisp
@@ -24,15 +24,18 @@
 
 (defclass test-slotted (hw:component)
   ((one
-    :pins 16)
+    :pins 8)
    (two
     :pins 8
-    :role :control))
+    :role :control)
+   (three
+    :pins 16
+    :role :io))
   (:metaclass hw:metacomponent))
 
 
 (defclass test-subcomponents (hw:component)
-  ((three
+  ((four
     :pins 8)
    (c
     :initarg :c
@@ -45,3 +48,46 @@
 
 
 ;; ---------- Wiring ----------
+
+(test test-wire-slot
+  "Test we can wire two pin interface slots on the same class together."
+  (let ((tc (make-instance 'test-slotted))
+	(b (make-instance 'hw:bus :width 8)))
+    (hw:connector-pins-connect (slot-value tc 'one) b)
+    (hw:connector-pins-connect (slot-value tc 'two) b)
+
+    ;; bus can see the component
+    (let ((cs (hw:components-seen-by b)))
+      (is (equal (length cs) 1))
+      (is (member tc cs)))
+
+    ;; component doesn't see itself
+    (is (null (hw:components-seen-by tc)))))
+
+
+(test test-wire-slots
+  "Test we can wire two slots on different components together."
+  (let ((tc1 (make-instance 'test-slotted ))
+	(tc2 (make-instance 'test-slotted))
+	(b (make-instance 'hw:bus :width 8)))
+    (hw:connector-pins-connect (slot-value tc1 'one) b)
+    (hw:connector-pins-connect (slot-value tc2 'two) b)
+
+    ;; bus can see both component
+    (let ((cs (hw:components-seen-by b)))
+      (is (equal (length cs) 2))
+      (is (member tc1 cs))
+      (is (member tc2 cs)))
+
+    ;; components see each other, but not themselves
+    (is (equal (hw:components-seen-by tc1) (list tc2)))
+    (is (equal (hw:components-seen-by tc2) (list tc1)))))
+
+
+(test test-slot-incomptible-widths
+  "Test we can't connect slots with unequal widths."
+  (let ((tc (make-instance 'test-slotted))
+	(b (make-instance 'hw:bus :width 8)))
+    (hw:connector-pins-connect (slot-value tc 'one) b)
+    (signals (hw:incompatible-pin-widths)
+      (hw:connector-pins-connect (slot-value tc 'three) b))))
