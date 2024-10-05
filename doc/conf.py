fd202ab492420545414f3a988afa5f003cadbf3a
Simon Dobson <simonininreland@gmail.com>
2024-10-05 14:49:49 +0100

    Grab symbols from cl-vhdsl

--- doc/conf.py
+++ doc/conf.py
@@ -39,7 +39,7 @@ cl_systems = [{"name": "cl-vhdsl",
                "path": join(dirname(realpath(__file__)), "../")}]
 
 # Packages to grab symbols from (in the core image)
-cl_packages = ["common-lisp"]
+cl_packages = ["cl-vhdsl"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
