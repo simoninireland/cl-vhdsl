913fa4fe06c10f7ed3648ef5e30e0298f1980720
Simon Dobson <simonininreland@gmail.com>
2025-01-04 17:27:03 +0000

    Modernised

--- src/def/package.lisp
+++ src/def/package.lisp
@@ -1,6 +1,6 @@
-;; Package definition for the component definition package
+;; Package definition for component definitions
 ;;
-;; Copyright (C) 2024 Simon Dobson
+;; Copyright (C) 2024-2025 Simon Dobson
 ;;
 ;; This file is part of cl-vhdsl, a Common Lisp DSL for hardware design
 ;;
@@ -20,85 +20,29 @@
 (in-package :common-lisp-user)
 
 (defpackage cl-vhdsl/def
-  (:use :cl :cl-bitfields :cl-vhdsl :cl-ppcre)
+  (:use :cl :alexandria :cl-vhdsl)
+  (:import-from :slot-extra-options
+		#:def-extra-options-metaclass
+		#:slot-exists-and-bound-p)
+  (:local-nicknames (:rtl :cl-vhdsl/rtl))
   (:export
-   ;; data types and type construtors
-   #:unsigned-bitfield
-   #:signed-bitfield
-   #:unsigned-8
-   #:signed-8
-   #:unsigned-16
-   #:signed-16
-   #:list-of-addressing-modes
-
    ;; Constants
    #:KB
    #:MB
 
-   ;; registers
-   #:register
-   #:register-name
-   #:register-width
-   #:data-register
-   #:address-register
-   #:index-register
-   #:special-register
-   #:program-counter
-   #:flag
-   #:flag-name
-   #:flag-register
-   #:flag-bit
-
-   ;; addressing modes
-   #:addressing-mode
-   #:addressing-mode-parse
-   #:addressing-mode-regexp
-   #:addressing-mode-bytes
-   #:addressing-mode-behaviour
-   #:implicit
-
-   ;; instructions
-   #:abstract-instruction
-   #:instruction
-   #:instruction-mnemonic
-   #:instruction-addressing-mode
-   #:instruction-addressing-modes
-   #:instruction-opcode
-   #:instruction-check
-   #:instruction-addressing-mode-bytes
-   #:instruction-bytes
-   #:instruction-assemble
-   #:instruction-add-class-for-mnemonic
-   #:instruction-class-for-mnemonic
-   #:instruction-behaviour
-   #:instruction-argument
-
-   ;; assembler
-   #:assembler-get-mnemonic
-   #:assembler-make-mnemonic-regexp
-   #:assembler-get-addressing-mode
-   #:assembler-make-addressing-modes-regexp
-
-   ;; architecture description
-   #:architecture
+   ;; components
    #:component
-   #:architecture-components
+   #:on-startup
+   #:pin-interface
+   #:subcomponent-interface
+   #:wiring-diagram
 
-   ;; cores
-   #:core
-   #:core-registers
-   #:core-flags
-   #:core-instruction-set
-
-   ;; memory
-   #:memory
-   #:memory-size
-
-   ;; buses
-   #:bus
-   #:bus-connections
+   ;; mixins and their behaviours
+   #:clocked
+   #:on-clock
+   #:resetable
+   #:on-reset
 
    ;; conditions
-   #:bad-addressing-mode
-   #:unknown-mnemonic
+
    ))
