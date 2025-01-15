d5bb7a8809ebd9bafbeb67877ad369d8d48e1c2f
Simon Dobson <simonininreland@gmail.com>
2025-01-15 19:23:27 +0000

    Added type-checks for @ blocks

--- test/test-rtl-control-flow.lisp
+++ test/test-rtl-control-flow.lisp
@@ -30,3 +30,49 @@
 			(setf a 5)
 			(setf b 34))
 		      :inblock)))
+
+
+
+;; ---------- Trigger expressions ----------
+
+(test test-typecheck-edges
+  "Test we can type-check edge trigger expressions."
+  (is (subtypep (rtl:typecheck '(let ((clk 0 :as :wire)
+				      (a 0))
+				 (rtl::@ ((rtl::posedge clk)))
+				 (setq a clk))
+			       emptyenv)
+		'(rtl::fixed-width-unsigned 1)))
+  )
+
+
+;; ---------- @ ----------
+
+(test test-typecheck-at
+  "Test we can type-check triggered blocks."
+  ;; single wire sensitivity
+  (is (subtypep (rtl:typecheck '(let ((clk 0 :as :wire)
+				      (a 0))
+				 (rtl::@ ((rtl::posedge clk))
+				  (setf a 1)))
+			       emptyenv)
+		'(rtl::fixed-width-unsigned 1)))
+
+  ;; multiple wires sensitivity
+  (is (subtypep (rtl:typecheck '(let ((clk 0 :as :wire)
+				      (rst 0 :as :wire)
+				      (a 0))
+				 (rtl::@ ((rtl::posedge clk) (rtl::negedge rst))
+				  (setf a 1)))
+			       emptyenv)
+		'(rtl::fixed-width-unsigned 1))))
+
+
+(test test-typecheck-at-wide
+  "Test we can detect too many wires in sensitivity."
+  (signals (rtl:type-mismatch)
+    (rtl:typecheck '(let ((clk 0 :width 4 :as :wire)
+			  (a 0))
+		     (rtl::@ ((rtl::posedge clk))
+		      (setf a 1)))
+		   emptyenv)))
