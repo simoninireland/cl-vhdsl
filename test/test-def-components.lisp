9dacaa447792daee265f735f1a8ba337f3c8cddb
Simon Dobson <simonininreland@gmail.com>
2025-01-28 15:42:07 +0000

    Reflecting new definitions

--- test/test-def-components.lisp
+++ test/test-def-components.lisp
@@ -27,26 +27,44 @@
   (defclass mop-create-simple (def:component)
     ((clk
       :width 1
-      :role :control
+      :role :clock
+      :exported t
       :reader clk)
+     (addr-width
+      :as :parameter
+      :reader addr-width)
      (addr
       :width 8
       :exported t
-      :accessor addr))
+      :accessor addr)
+     (sub
+      :type def:component
+      :accessor sub))
     (:metaclass def:synthesisable-component))
 
   (let ((c (make-instance 'mop-create-simple)))
+
+    ;; check the pin interface (exported pins)
     (is (set-equal (def::pin-interface (class-of c))
-		   '(addr)))
-    (is (set-equal (def::variables (class-of c))
-		   '(def::name clk)))
+		   '(addr clk)))
 
+    ;; check clock is defined and only a wire
     (is (= (def::slot-width (class-of c) 'clk) 1))
-    (is (eql (def::slot-role (class-of c) 'clk) :control))
+    (is (eql (def::slot-role (class-of c) 'clk) :clock))
 
+    ;; check parameters
+    (is (set-equal (def::parameters (class-of c)) '(addr-width)))
+
+    ;; check addr has the right attributes
     (is (def::slot-exported (class-of c) 'addr))
     (is (= (def::slot-width (class-of c) 'addr) 8))
     (is (null (def::slot-role (class-of c) 'addr)))
 
-    (is (equal (def::synthesise-module-args (class-of c))
-	       '((addr :width 8))))))
+    ;; check sub-components
+    (is (set-equal (def::subcomponents (class-of c)) '(sub)))
+
+    ;; check only addr are clk are exported
+    (is (set-equal (def::synthesise-module-args (class-of c))
+		   '((addr :width 8)
+		     (clk :width 1))
+		   :test #'equal))))
