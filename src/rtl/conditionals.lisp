0fcb4b936b9b3addd5d40dcc134d178d33ffe133
Simon Dobson <simonininreland@gmail.com>
2025-02-11 17:19:52 +0000

    Fixed copyright notice

--- src/rtl/conditionals.lisp
+++ src/rtl/conditionals.lisp
@@ -1,5 +1,14 @@
 ;; Synthesisable conditionals
 ;;
+;; Copyright (C) 2024--2025 Simon Dobson
+;;
+;; This file is part of cl-vhdsl, a Common Lisp DSL for hardware design
+;;
+;; cl-vhdsl is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
 ;; cl-vhdsl is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
