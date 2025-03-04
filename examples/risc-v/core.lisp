dd93b4487480b63039fa7dbf77bb51756adf1072
Simon Dobson <simonininreland@gmail.com>
2025-03-04 17:53:50 +0000

    Removed use of with-bitfields to get closer to source

--- examples/risc-v/core.lisp
+++ examples/risc-v/core.lisp
@@ -50,154 +50,152 @@
     (setq leds-out leds)
 
     ;; instruction decoding
-    (with-bitfields (i i i i i i i)
-	instr
-      (let-wires ((isALUreg (= i #2r0110011) :width 1)
-		  (isALUimm (= i #2r0010011) :width 1)
-		  (isBranch (= i #2r1100011) :width 1)
-		  (isJALR   (= i #2r1100111) :width 1)
-		  (isJAL    (= i #2r1101111) :width 1)
-		  (isAIUPC  (= i #2r0010111) :width 1)
-		  (isLUT    (= i #2r0110111) :width 1)
-		  (isLoad   (= i #2r0000011) :width 1)
-		  (isStore  (= i #2r0100011) :width 1)
-		  (isSystem (= i #2r1110011) :width 1)
+    (let-wires ((isALUreg (= (bits instr 7) #2r0110011) :width 1)
+		(isALUimm (= (bits instr 7) #2r0010011) :width 1)
+		(isBranch (= (bits instr 7) #2r1100011) :width 1)
+		(isJALR   (= (bits instr 7) #2r1100111) :width 1)
+		(isJAL    (= (bits instr 7) #2r1101111) :width 1)
+		(isAIUPC  (= (bits instr 7) #2r0010111) :width 1)
+		(isLUT    (= (bits instr 7) #2r0110111) :width 1)
+		(isLoad   (= (bits instr 7) #2r0000011) :width 1)
+		(isStore  (= (bits instr 7) #2r0100011) :width 1)
+		(isSystem (= (bits instr 7) #2r1110011) :width 1)
 
-		  ;; intermediate formats
-		  (Uimm (make-bitfields (bit instr 31)
-					(bits instr 30 :end 12)
-					(extend-bits 0 12))
-			:width 32)
-		  (Iimm (make-bitfields (extend-bits (bit instr 31) 21)
-					(bits instr 30 :end 20))
-			:width 32)
-		  (Simm (make-bitfields (extend-bits (bit instr 31) 21)
-					(bits instr 30 :end 25)
-					(bits instr 11 :end 7))
-			:width 32)
-		  (Bimm (make-bitfields (extend-bits (bit instr 31) 20)
-					(bit instr 7)
-					(bits instr 30 :end 25)
-					(bits instr 11 :end 8)
-					(extend-bits 0 1))
+		;; intermediate formats
+		(Uimm (make-bitfields (bit instr 31)
+				      (bits instr 30 :end 12)
+				      (extend-bits 0 12))
+		      :width 32)
+		(Iimm (make-bitfields (extend-bits (bit instr 31) 21)
+				      (bits instr 30 :end 20))
+		      :width 32)
+		(Simm (make-bitfields (extend-bits (bit instr 31) 21)
+				      (bits instr 30 :end 25)
+				      (bits instr 11 :end 7))
+		      :width 32)
+		(Bimm (make-bitfields (extend-bits (bit instr 31) 20)
+				      (bit instr 7)
+				      (bits instr 30 :end 25)
+				      (bits instr 11 :end 8)
+				      (extend-bits 0 1))
 
-			:width 32)
-		  (Jimm (make-bitfields (extend-bits (bit instr 31) 12)
-					(bits instr 19 :end 12)
-					(bit instr 20)
-					(bits instr 30 :end 21)
-					(extend-bits 0 1))
-			:width 32)
+		      :width 32)
+		(Jimm (make-bitfields (extend-bits (bit instr 31) 12)
+				      (bits instr 19 :end 12)
+				      (bit instr 20)
+				      (bits instr 30 :end 21)
+				      (extend-bits 0 1))
+		      :width 32)
 
-		  ;; source and destination registers
-		  (rs1Id (bits instr 19 :end 15) :width 5)
-		  (rs2Id (bits instr 24 :end 20) :width 5)
-		  (rdId  (bits instr 11 :end 7)  :width 5)
+		;; source and destination registers
+		(rs1Id (bits instr 19 :end 15) :width 5)
+		(rs2Id (bits instr 24 :end 20) :width 5)
+		(rdId  (bits instr 11 :end 7)  :width 5)
 
-		  ;; function codes
-		  (funct3 (bits instr 14 :end 12) :width 3)
-		  (funct7 (bits instr 31 :end 25) :width 7))
+		;; function codes
+		(funct3 (bits instr 14 :end 12) :width 3)
+		(funct7 (bits instr 31 :end 25) :width 7))
 
-	;; register bank
-	(let ((RegisterBank  (make-array '(32) :element-type (fixed-width-unsigned 32)))
-	      (rs1           0 :width 32)
-	      (rs2           0 :width 32)
-	      (writeBackData 0 :width 32 :as :wire)
-	      (writeBackEn   0 :width 1  :as :wire))
+	       ;; register bank
+	       (let ((RegisterBank  (make-array '(32) :element-type (fixed-width-unsigned 32)))
+		     (rs1           0 :width 32)
+		     (rs2           0 :width 32)
+		     (writeBackData 0 :width 32 :as :wire)
+		     (writeBackEn   0 :width 1  :as :wire))
 
-	  ;; the ALU
-	  (let-wires ((aluIn1 rs1 :width 32)
-		      (aluIn2 (if isALUreg
-				  rs2
-				  Iimm)
-			      :width 32)
-		      (shamt (if isALUreg
-				 (bits rs2 4)
-				 (bits instr 24 :end 20))
-			     :width 5))
+		 ;; the ALU
+		 (let-wires ((aluIn1 rs1 :width 32)
+			     (aluIn2 (if isALUreg
+					 rs2
+					 Iimm)
+				     :width 32)
+			     (shamt (if isALUreg
+					(bits rs2 4)
+					(bits instr 24 :end 20))
+				    :width 5))
 
-	    (let-registers ((aluOut 0 :width 32))
+			    (let-registers ((aluOut 0 :width 32))
 
-	      (@ (*)
-		 (case funct3
-		   (#2r000
-		    (if (logand (bit funct7 5)
-				(bit instr 5))
-			(setq aluOut (- aluIn1 aluIn2) :sync t)
-			(setq aluOut (+ aluIn1 aluIn2) :sync t)))
+					   (@ (*)
+					      (case funct3
+						(#2r000
+						 (if (logand (bit funct7 5)
+							     (bit instr 5))
+						     (setq aluOut (- aluIn1 aluIn2) :sync t)
+						     (setq aluOut (+ aluIn1 aluIn2) :sync t)))
 
-		   (#2r001
-		    (setq aluOut (<< aluIn1 shamt) :sync t))
+						(#2r001
+						 (setq aluOut (<< aluIn1 shamt) :sync t))
 
-		   (#2r010
-		    (setq aluOut (< aluIn1 aluIn2) :sync t)) ;; signed
+						(#2r010
+						 (setq aluOut (< aluIn1 aluIn2) :sync t)) ;; signed
 
-		   (#2r011
-		    (setq aluOut (< aluIn1 aluIn2) :sync t)) ;; unsigned
+						(#2r011
+						 (setq aluOut (< aluIn1 aluIn2) :sync t)) ;; unsigned
 
-		   (#2r100
-		    (setq aluOut (logxor aluIn1 aluIn2) :sync t))
+						(#2r100
+						 (setq aluOut (logxor aluIn1 aluIn2) :sync t))
 
-		   (#2r101
-		    (if (bit funct7 5)
-			(setq aluOut (>> aluIn1 shamt) :sync t) ;; sign-extended
-			(setq aluOut (>> aluIn1 shamt) :sync t))) ;; unsigned
+						(#2r101
+						 (if (bit funct7 5)
+						     (setq aluOut (>> aluIn1 shamt) :sync t) ;; sign-extended
+						     (setq aluOut (>> aluIn1 shamt) :sync t))) ;; unsigned
 
-		   (#2r110
-		    (setq aluOut (logior aluIn1 aluIn2) :sync t))
+						(#2r110
+						 (setq aluOut (logior aluIn1 aluIn2) :sync t))
 
-		   (#2r111
-		    (setq aluOut (logand aluIn1 aluIn2) :sync t))))
+						(#2r111
+						 (setq aluOut (logand aluIn1 aluIn2) :sync t))))
 
-	      ;; the state machine
-	      (let ((FETCH-INSTR 0 :as :constant)
-		    (FETCH-REGS  1 :as :constant)
-		    (EXECUTE     2 :as :constant)
-		    (state       0 :width 3))
+					   ;; the state machine
+					   (let ((FETCH-INSTR 0 :as :constant)
+						 (FETCH-REGS  1 :as :constant)
+						 (EXECUTE     2 :as :constant)
+						 (state       0 :width 3))
 
-		(let ((nextpc (cond (isJAL
-				     (+ pc Jimm))
-				    (isJALR
-				     (+ rs1 Iimm))
-				    (t
-				     (+ PC 4)))
-			      :width 32))
+					     (let ((nextpc (cond (isJAL
+								  (+ pc Jimm))
+								 (isJALR
+								  (+ rs1 Iimm))
+								 (t
+								  (+ PC 4)))
+							   :width 32))
 
-		  (setq writeBackData (if (or isJAL isJALR)
-					  (+ pc 4)
-					  aluOut))
-		  (setq writeBackEn (and (= state 2)
-					 (or isALUReg
-					     isALUImm
-					     isJAL
-					     isJALR)))
+					       (setq writeBackData (if (or isJAL isJALR)
+								       (+ pc 4)
+								       aluOut))
+					       (setq writeBackEn (and (= state 2)
+								      (or isALUReg
+									  isALUImm
+									  isJAL
+									  isJALR)))
 
-		  (@ (posedge clk)
-		     (if reset
-			 (progn
-			   (setq pc 0)
-			   (setq state FETCH-INSTR))
+					       (@ (posedge clk)
+						  (if reset
+						      (progn
+							(setq pc 0)
+							(setq state FETCH-INSTR))
 
-			 (if (and writeBackEn
-				  (/= rdId 0))
-			     (progn
-			       (setf (aref RegisterBank rdId) writeBackData)
+						      (if (and writeBackEn
+							       (/= rdId 0))
+							  (progn
+							    (setf (aref RegisterBank rdId) writeBackData)
 
-			       ;; update the LEDS if writing to R1
-			       (if (= rdId 1)
-				   (setq leds writeBackData)))))
+							    ;; update the LEDS if writing to R1
+							    (if (= rdId 1)
+								(setq leds writeBackData)))))
 
-		     (case state
-		       (FETCH-INSTR
-			(setq instr (aref mem (bits pc 31 :end 2)))
-			(setq state FETCH-REGS))
+						  (case state
+						    (FETCH-INSTR
+						     (setq instr (aref mem (bits pc 31 :end 2)))
+						     (setq state FETCH-REGS))
 
-		       (FETCH-REGS
-			(setq rs1 (aref RegisterBank rs1Id))
-			(setq rs2 (aref RegisterBank rs2Id))
-			(setq state EXECUTE))
+						    (FETCH-REGS
+						     (setq rs1 (aref RegisterBank rs1Id))
+						     (setq rs2 (aref RegisterBank rs2Id))
+						     (setq state EXECUTE))
 
-		       (EXECUTE
-			(if (not isSystem)
-			    (setq pc nextpc))
-			(setq state FETCH-INSTR)))))))))))))
+						    (EXECUTE
+						     (if (not isSystem)
+							 (setq pc nextpc))
+						     (setq state FETCH-INSTR))))))))))))
