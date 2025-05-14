da04cf0b819151ed16be7ce1aec9f26722e4b740
Simon Dobson <simonininreland@gmail.com>
2025-05-14 15:38:08 +0100

    First version of emulator in idiomatic Lisp

---
+++ examples/risc-v/emu/straight.lisp
@@ -0,0 +1,210 @@
+;; 32-bit integer-only RISC-V core in emulation
+;;
+;; Copyright (C) 2024--2025 Simon Dobson
+;;
+;; This file is part of cl-vhdsl, a Common Lisp DSL for hardware design
+;;
+;; cl-vhdsl is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; cl-vhdsl is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with cl-vhdsl. If not, see <http://www.gnu.org/licenses/gpl.html>.
+
+(in-package :cl-vhdsl/examples/risc-v/emu)
+
+
+(defun alu (a b op add/sub)
+  "Perform OP on A and B.
+
+ADD/SUB selects between add (0) and subtract (1), or logical (0) and arithmetic shift (1)."
+  (case op
+    (#2r000
+     ;; add or subtract
+     (if (= add/sub 1)
+	 (- a b)
+	 (+ a b)))
+
+    (#2r001
+     ;; left shift
+     (ash a b))
+
+    (#2r010
+     ;; less-than unsigned
+     (< (coerce a '(unsigned-byte 32))    ; TESTME
+	(coerce b '(unsigned-byte 32))))
+
+    (#2r011
+     ;; less-than signed
+     (< (coerce a '(signed-byte 32))      ; TESTME
+	(coerce b '(signed-byte 32))))
+
+    (#2r100
+     ;; exclusive or
+     (logxor a b))
+
+    (#2r101
+     ;; right shift, logical or arithmetic
+     (if (= add/sub 1)
+	 (ash a b)            ; FIXME: get the sdign extension right
+	 (ash a (- b))))
+
+    (#2r110
+     ;; or
+     (logior a b))
+
+    (#2r100
+     ;; and
+     (logand a b))))
+
+
+(defun whitespace-char-p (x)
+  (or (char= #\space x)
+      (not (graphic-char-p x))))
+
+
+(defun skip-whitespace (str)
+  "Skip whitespace in STR."
+  (when (whitespace-char-p (peek-char nil str))
+    (read-char str)
+    (skip-whitespace str)))
+
+
+(defun load-program (mem filename)
+  "Load a program from FILENAME into MEM."
+  (let ((pc 0))
+    (labels ((read-instruction (str)
+	       (let ((instr (read-integer str t t nil :radix 16 :unsigned-number t)))
+		 (setf (aref mem (ash pc -2)) instr)
+		 (incf pc 4)))
+
+	     (read-instructions (str)
+	       (handler-case
+		   (progn
+		     (skip-whitespace str)
+		     (read-instruction str)
+		     (read-instructions str))
+
+		 (end-of-file (eof)
+		   t))))
+
+      (with-open-file (str filename :direction :input)
+	(read-instructions str)))))
+
+
+(defun rv32i (firmware-name cycles)
+  "A RISC-V 32-bit integer core emulator."
+  (declare (optimize debug))
+
+  (let ((clk 0)
+	(mem (make-array '(256) :element-type '(unsigned-byte 32)
+				:initial-element 0))
+	(register-file (make-array '(32) :element-type '(unsigned-byte 32)
+					 :initial-element 0))
+	(pc 0))
+
+    ;; load the firmware
+    (load-program mem firmware-name)
+
+    ;; run the core
+    (dotimes (c cycles)
+      ;; instruction fetch
+      (let ((instr (aref mem (ash pc -2)))
+	    write-back
+	    next-pc)
+
+	;; instruction decoding
+	(with-bitfields ((funct7 7) (rs2id 5) (rs1id 5) (funct3 3) (rdid 5) (opcode 7))
+	    instr
+
+	  ;; increment PC
+	  (setq next-pc (+ pc 4))
+
+	  (case opcode
+	    (#2r0110011
+	     ;; ALU register
+	     (let ((rs1 (aref register-file rs1id))
+		   (rs2 (aref register-file rs2id)))
+
+	       (setf write-back
+		     (alu rs1 rs2
+			  funct3
+			  (logand (1bit funct7 5)
+				  (1bit instr 5))))))
+
+	    (#2r0010011
+	     ;; ALU immediate
+	     (let ((rs1 (aref register-file rs1id))
+		   (Iimm (coerce (nbits instr 31 :end 20) '(signed-byte 12))))
+
+	       (setf write-back
+		     (alu rs1 Iimm
+			  funct3
+			  (logand (1bit funct7 5)
+				  (1bit instr 5))))))
+
+	    (#2r1100011
+	     ;; branch
+	     )
+
+	    (#2r1100111
+	     ;; jump and link relative to register
+	     (let ((rs1 (aref register-file rs1id))
+		   (Iimm (coerce (nbits instr 31 :end 20) '(signed-byte 12))))
+
+	       (setf write-back (+ pc 4))
+	       (setf next-pc (+ rs1 Iimm) )))
+
+	    (#2r1101111
+	     ;; jump and link relative to PC
+	     (let ((Jimm (make-bitfields (signed-byte 32)
+					 ((copy-bit (1bit instr 31) 12) 12)
+					 ((nbits instr 19 :end 12) 8)
+					 ((1bit instr 20) 1)
+					 ((nbits instr 30 :end 21) 10)
+					 (0 1))))
+
+	       (setf write-back (+ pc 4))
+	       (setf next-pc (+ pc Jimm))))
+
+	    (#2r0010111
+	     ;; add upper immediate
+	     )
+
+	    (#2r0110111
+	     ;; load upper
+	     )
+
+	    (#2r0000011
+	     ;; load
+	     )
+
+	    (#2r0100011
+	     ;; store
+	     )
+
+	    (#2r1110011
+	     ;; system
+	     ))
+
+	  ;; write-back register
+	  (setf (aref register-file rdid) write-back))
+
+	;; update PC
+	(setf pc next-pc)))
+
+    (aref register-file 1)
+
+
+
+    )
+
+
+
+  )
---
+++ examples/risc-v/emu/utils.lisp
@@ -0,0 +1,185 @@
+;; 32-bit integer-only RISC-V core helper macros and functions
+;;
+;; Copyright (C) 2024--2025 Simon Dobson
+;;
+;; This file is part of cl-vhdsl, a Common Lisp DSL for hardware design
+;;
+;; cl-vhdsl is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; cl-vhdsl is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with cl-vhdsl. If not, see <http://www.gnu.org/licenses/gpl.html>.
+
+(in-package :cl-vhdsl/examples/risc-v/emu)
+
+(defun ensure-one-of (&rest args)
+  "Ensure exactly one of ARGS is non-nil."
+  (let ((non-nils (foldr (lambda (n v)
+			   (if v
+			       (1+ n)
+			       n))
+			 args 0)))
+
+    (cond ((= non-nils 0)
+	   (error "Missing required argument"))
+	  ((> non-nils 1)
+	   (error "Too many arguments"))
+	  (t t))))
+
+
+(defun nbits (expr start &key width end)
+  "Extract bits from EXPR starting at bit START.
+
+The number of bits can be given by WIDTH for a number of bits
+or END for the last bit. One or other must be supplied."
+  (ensure-one-of width end)
+
+  (unless end
+    (setq end (1+ (- start width))))
+  (unless width
+    (setq width (1+ (- start end))))
+
+  (logand (ash expr (- end))
+	  (mask width)))
+
+
+(defun 1bit (expr b)
+  "Extract bit B from EXPR."
+  (if (= (logand expr
+		 (ash 1 b))
+	 0)
+      0
+      1))
+
+
+(defun copy-bit (b w)
+  "Return a number that is W copies of the bit B."
+  (if (= 0 b)
+      0
+      (1- (ash b w))))
+
+
+(defun mask (w)
+  "Return a mask of W 1s."
+  (1- (ash 1 w)))
+
+
+(defmacro with-bitfields (fields expr &body body)
+  "Bind FIELDS of EXPR in BODY.
+
+Each element of FIELDS should be a list consisting of a symbol and
+a width. These variables are created by extracting the corresponding bits
+from EXPR, with the last variable coming from the lowest-order bits.
+The variables are then in scope (as generalised places) in BODY."
+  (with-gensyms (val)
+    (labels ((field-variables (fields start)
+	       (if (null fields)
+		   ()
+		   (let ((field (car fields)))
+		     (destructuring-bind (var width)
+			 field
+		       (cons `(,var (nbits ,val ,start :width ,width))
+			     (field-variables (cdr fields) (- start width)))))))
+
+	     (fields-width (fields)
+	       (foldr (lambda (i nw)
+			(+ (cadr nw) i))
+		      fields 0)))
+
+      (let ((vars (field-variables fields (1- (fields-width fields)))))
+	`(let ((,val ,expr))
+	   (symbol-macrolet ,vars
+	     ,@body))))))
+
+
+(defun interleave (&rest ls)
+  "Construct a list from the interleaved values of the lists LS.
+
+For example, (INTERLEAVE '(1 2 3) '(a b c)) will construct
+(1 a 2 b 3 c). The lists must all be of equal length."
+  (labels ((take-cars (ls)
+	     (cond ((every #'null ls)
+		    ())
+		   ((some #'null ls)
+		    (error "Lists not the same length"))
+		   (t
+		    (let ((cs (mapcar #'car ls)))
+		      (append cs (take-cars (mapcar #'cdr ls))))))))
+
+    (if (null ls)
+	()
+	(take-cars ls))))
+
+
+(defun twos-complement (n w)
+  "If N is negative under two's complement, return the negative number.
+
+Otherwise return N unchanged. W is the width of N in bits."
+  (if (= (1bit n (1- w)) 1)
+      ;; number is negative, invert all bits, add 1, and make negative
+      (- (1+ (logxor (logand n (mask (1- w))) (mask (1- w)))))
+
+      ;; number is positive, return unchanged
+      n))
+
+
+(defmacro make-bitfields (ty &rest fields)
+  "Compose FIELDS into a value of type TY.
+
+Each element of FIELDS should be a list consisting of an expression
+and a width. The bitfield is constructed by concatenating the bits
+from the expressions, with the highest-order bits coming first. The
+bitfield is then coerced to be of type TY."
+  (labels ((field-bits (field width)
+	     "Turn FIELD into a declaration with WIDTH declaration as a mask."
+	     (let ((val (car field)))
+	       (let ((var (gensym)))
+		 ;; mask generated from the variable name of WIDTH
+		 `(,var (logand ,val (1- (ash 1 ,(car width))))))))
+
+	   (field-widths (field)
+	     "Turn FIELD into a declaration of its width in a new variable."
+	     (destructuring-bind (val width)
+		 field
+	       (let ((var (gensym)))
+		 `(,var ,width))))
+
+	   (field-shifts (vars widths result shift-p)
+	     "Turn lists of field and width variables into an expression."
+	     (if (null vars)
+		 nil
+		 (let ((var (car vars)))
+		   (let* ((width (car widths))
+			  (nextexpr (field-shifts (cdr vars) (cdr widths) result t))
+			  (shifter (if shift-p
+				       `(ash ,result ,width)
+				       result)))
+		     (cons `(setq ,result (+ ,var ,shifter))
+			   nextexpr))))))
+
+    (let* ((widths (mapcar #'field-widths fields))
+	   (vals (mapcar #'field-bits fields widths))
+	   (val-vars (mapcar #'car vals))
+	   (width-vars (mapcar #'car widths)))
+
+      (with-gensyms (bitfield bitfield-width)
+	`(let* (,@(interleave widths
+			      vals)
+		(,bitfield-width (+ ,@ (mapcar #'cadr widths)))
+		(,bitfield 0))
+	   ;; add the results into the bitfield
+	   ,@(field-shifts val-vars width-vars bitfield nil)
+
+	   ,(if (signed-byte-p ty)
+		;; signed type, do the two's-complement conversion
+		`(twos-complement ,bitfield ,bitfield-width)
+
+		;; otherwise leave as-is
+		bitfield))))))
